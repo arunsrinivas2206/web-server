94b3152bb5c77a4abdd66c4f759492c4220f939b
arun2206 <asrinivas@mobiquityinc.com>
2017-08-15 16:07:05 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+
+app.set('view engine', 'hbs');
+
+hbs.registerHelper("getCurrentYear", () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper("uppercase", (text) => {
+	return text.toUpperCase();
+});
+
+// app.use((req, res, next) => {
+// 	res.render('maintenance.hbs');
+// });
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Middleware
+app.use((req, res, next) => {
+	console.log(`${new Date()}: ${req.url} - ${req.method}`);
+	fs.appendFile('logs.txt', `${new Date()}: ${req.url} - ${req.method}\n`, (err) => {
+		return console.log(err);
+	});
+	next();
+});
+
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', {title: 'Home page', name: 'Arun', pageTitle: 'Home'});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {title: 'About', name: 'Arun', pageTitle: 'About'});
+});
+
+app.get('/bad', (req, res) => {
+	res.send({
+		error: "Unable to process the request"
+	});
+});
+
+app.listen(8000, () => {
+	console.log('listening on port 8000');
+});
+
